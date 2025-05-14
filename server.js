c97a1765ce56c9866e9e5739831861b493221f22
SabbirAhmad <sabbir.ahmad1@g.bracu.ac.bd>
2025-05-14 19:12:57 +0600

    added mongoDB

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
